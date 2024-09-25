41e558658e77df51f798eeeef9e273c349f008e3
Yossi Miller <y0504199881@gmail.com>
2024-09-26 00:25:25 +0300

    first commit

---
+++ blueprints/main_blueprint.py
@@ -0,0 +1,21 @@
+# TODO: change file name and blueprints name
+import selectors
+
+from flask import jsonify, request, blueprints
+
+from services.main_server import select_all_with_psycopg2
+
+main = blueprints.Blueprint('main', __name__, url_prefix='/')
+
+@main.route('/get')
+def get_with_psycopg2():
+    request_info = {
+        "ip": request.remote_addr,
+        "endpoint": request.url,
+        "method": request.method
+    }
+    result = select_all_with_psycopg2(request_info)
+    if result:
+        return jsonify(result), 200
+    else:
+        return jsonify({'message': 'No data'}), 404
